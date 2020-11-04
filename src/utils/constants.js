d069fe3ac7e98e7ee1e5bc3c3235907762d69461
achraf <achrafachkari@hotmail.fr>
2020-11-04 12:42:23 +0100

    Fix Teams Pagincation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const LEAGUES_PER_PAGE = 5;
 export const TEAMS_PER_PAGE = 5;
 export const API_TOKEN = process.env.REACT_APP_API_TOKEN;
-export const API = "https://api.pandascore.co"
+// export const API = "https://api.pandascore.co"
+export const API = "/api"
